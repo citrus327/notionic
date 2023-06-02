cb1c02aab04204821773ec7cf74ab099576a4188
citrus327 <citrus327@outlook.com>
2023-06-02 09:47:45 +0800

    feat: switch pay

--- components/Post/WechatPay.js
+++ components/Post/WechatPay.js
@@ -3,12 +3,13 @@ import Image from 'next/image'
 // export default function WechatPay() {
 const WechatPay = () => {
   return (
-    <div className='fixed inline-flex shadow-lg bg-gray-100 dark:bg-gray-400 p-5 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+    <div className='fixed inline-flex shadow-lg bg-gray-100 dark:bg-gray-400  rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
       <Image
         src='/wechat-pay.png'
         alt='WeChat Pay'
         width={200}
         height={200}
+        style={{ borderRadius: '9999px' }}
       />
     </div>
   )
